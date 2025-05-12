bba89c3a88b60310595bc4b0631c2ba135ffcc8f
Quinn Welton <quinn.welton@gmail.com>
2025-05-11 17:45:26 -0700

    Trying to solve 404 error in api.js, env file is missing, switching branches to investigate.

--- movieDatabaseQuinn/src/pages/PageHome.jsx
+++ movieDatabaseQuinn/src/pages/PageHome.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import { getPopularMovies } from "../utilities/api"
+
 function PageHome() {
+
+    const [popularMovies, setPopularMovies] = useState([])
+
+    useEffect(() => {
+        getPopularMovies()
+        .then((date) => {
+            console.log(data);
+        })
+    }, [])
+
   return (
     <div>PageHome</div>
   )
--- movieDatabaseQuinn/src/utilities/api.js
+++ movieDatabaseQuinn/src/utilities/api.js
@@ -1,2 +1,17 @@
-const API_URL = "https://api.themoviedb.org/3/movie/";
+const API_URL = "https://api.themoviedb.org/3/movie";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+function getPopularMovies() {
+    return fetch(`${API_URL}/popular?api_keys=${API_KEY }`)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("Network Response Code was not OK")
+        }
+        return response.json()
+    })
+    .catch((error) => {
+        throw error;
+    });
+}
+
+export { getPopularMovies };
